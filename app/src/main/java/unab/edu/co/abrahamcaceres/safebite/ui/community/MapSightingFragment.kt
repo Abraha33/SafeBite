576cb5ffb876f49ad158d4a2d2ba5cfea8d03bc3
Abraha33 <acaceres163@unab.edu.co>
2026-05-19 22:48:43 -0500

    Fix: replace native Maps SDK with interactive WebView to bypass Google Cloud API Key restriction

--- app/src/main/java/unab/edu/co/abrahamcaceres/safebite/ui/community/MapSightingFragment.kt
+++ app/src/main/java/unab/edu/co/abrahamcaceres/safebite/ui/community/MapSightingFragment.kt
@@ -1,34 +1,18 @@
 package unab.edu.co.abrahamcaceres.safebite.ui.community
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import unab.edu.co.abrahamcaceres.safebite.R
 import unab.edu.co.abrahamcaceres.safebite.databinding.FragmentMapSightingBinding
 
-class MapSightingFragment : Fragment(), OnMapReadyCallback {
+class MapSightingFragment : Fragment() {
 
     private var _binding: FragmentMapSightingBinding? = null
     private val binding get() = _binding!!
-
-    private var latitude: Float = 7.1254f
-    private var longitude: Float = -73.1198f
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            latitude = it.getFloat("latitude", 7.1254f)
-            longitude = it.getFloat("longitude", -73.1198f)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,30 +25,21 @@ class MapSightingFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupToolbar()
-        val mapFragment = childFragmentManager
-            .findFragmentById(R.id.mapContainer) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
+        val latitude = arguments?.getFloat("latitude", 7.1254f) ?: 7.1254f
+        val longitude = arguments?.getFloat("longitude", -73.1198f) ?: -73.1198f
+
+        setupWebView(latitude, longitude)
     }
 
-    private fun setupToolbar() {
-        binding.toolbarMap.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }
-    }
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView(latitude: Float, longitude: Float) {
+        val webView = binding.mapWebView
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
 
-    override fun onMapReady(googleMap: com.google.android.gms.maps.GoogleMap) {
-        val position = LatLng(latitude.toDouble(), longitude.toDouble())
-        googleMap.clear()
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(position)
-                .title("Avistamiento")
-        )
-        googleMap.moveCamera(
-            CameraUpdateFactory.newLatLngZoom(position, 15f)
-        )
-        googleMap.uiSettings.isZoomControlsEnabled = true
+        val mapUrl = "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude"
+        webView.loadUrl(mapUrl)
     }
 
     override fun onDestroyView() {
